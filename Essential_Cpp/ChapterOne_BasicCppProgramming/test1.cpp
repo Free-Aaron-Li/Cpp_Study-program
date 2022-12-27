8b47edf1bac0158a6167f0ff95b6447523e3e310
AaronLi <free_aaron.li@outlook.com>
2022-12-27 21:21:09 +0800

    第一个程序

--- Essential_Cpp/ChapterOne_BasicCppProgramming/test1.cpp
+++ Essential_Cpp/ChapterOne_BasicCppProgramming/test1.cpp
@@ -2,7 +2,7 @@
  * @file test1.cpp
  * @author AaronLi
  * @date 2022-12-25-下午10:44
- * @description 第一个代码
+ * @description 第一个代码,输入姓名
  * @link 
  */
 
@@ -13,14 +13,19 @@
 using namespace std;
 
 int main() {
-    string user_name;
+    string user_firstname;
+    string user_lastname;
     /* cout cin 为两个iostream类对象output 运算符 << */
     cout << "Please enter your first name: ";
     /* input 运算符 >> */
-    cin >> user_name;
+    cin >> user_firstname;
+    cout << "Please enter your last name: ";
+    cin >> user_lastname;
     cout << '\n'
          << "Hello, "
-         << user_name
+         << user_firstname
+         << " "
+         << user_lastname
          << " ... and goodbye \n";
 
     return 0;
