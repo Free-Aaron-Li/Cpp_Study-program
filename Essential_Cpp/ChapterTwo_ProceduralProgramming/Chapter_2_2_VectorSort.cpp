7e0b0b95b1515713d5fdd7b3fa802fbc4a6a1e79
AaronLi <free_aaron.li@outlook.com>
2023-01-19 00:07:41 +0800

    update

--- Essential_Cpp/ChapterTwo_ProceduralProgramming/Chapter_2_2_VectorSort.cpp
+++ Essential_Cpp/ChapterTwo_ProceduralProgramming/Chapter_2_2_VectorSort.cpp
@@ -13,5 +13,103 @@
 
 #include <iostream>
 #include <vector>
+
 using namespace std;
+
+void display(vector<int> &vec);
+
+void swap(int &val1, int &val2);
+
+void bubble_sort(vector<int> &vec);
+
+/**
+ * @date 23-1-18 下午6:37
+ * @description
+ * 显示vector
+ */
+
+void display(vector<int> &vec) {
+    for (int i : vec) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+/**
+ * @date 23-1-18 下午6:39
+ * @description
+ * 元素位置交换
+ */
+
+void swap(int &val1, int &val2) {
+    int temp = val1;
+    val1 = val2;
+    val2 = temp;
+}
+
+/**
+ * @date 23-1-18 下午6:41
+ * @description
+ * 冒泡排序
+ */
+
+void bubble_sort(vector<int> &vec) {
+    for (int i = 0; i < vec.size(); ++i) {
+        for (int j = i + 1; j < vec.size(); ++j) {
+            if (vec[i] > vec[j]) {
+                swap(vec[i], vec[j]);
+            }
+        }
+    }
+}
+
+int main() {
+    int a[8] = {8, 24, 4, 23, 25, 412, 3, 341};
+    /* vector(begin,end):复制[begin,end)区间内另一个数组的元素到vector中 */
+    vector<int> vec(a, a + 8);
+
+    display(vec);
+    bubble_sort(vec);
+
+    cout << "vector after sort: \n";
+    display(vec);
+    cout << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
