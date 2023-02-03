bcfc6e0dca6278f0688a2266e37944fba316c631
AaronLi <Free_Aaron.Li@outlook.com>
2023-02-03 11:34:46 +0800

    更新

---
+++ Essential_Cpp/ChapterThree_GenericProgramming/include/swap.hpp
@@ -0,0 +1,11 @@
+/**
+ * @file swap.hpp
+ * @author AaronLi
+ * @date 2023-02-03-上午10:47
+ * @description
+ */
+
+#ifndef CHAPTERTHREE_SWAP_HPP
+#define CHAPTERTHREE_SWAP_HPP
+
+#endif  // CHAPTERTHREE_SWAP_HPP
--- Essential_Cpp/ChapterThree_GenericProgramming/src/Chapter3_1_find.cpp
+++ Essential_Cpp/ChapterThree_GenericProgramming/src/Chapter3_1_find.cpp
@@ -10,7 +10,24 @@
 #include <iostream>
 using namespace std;
 
+void swap(int a,int b);
+
 int main() {
-  cout << "hello world!" << endl;
+  int a=10;
+  int b=20;
+  swap(a,b);
   return 0;
 }
+
+void swap(int a,int b) {
+  int temp = a;
+  a = b;
+  b = temp;
+  cout<<"a: "<<a<<endl;
+  cout<<"b: "<<b<<endl;
+}
+
+
+
+
+
