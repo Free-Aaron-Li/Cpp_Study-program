e14ad37af471a58a2975e681d5137864716b9bf3
AaronLi <Free_Aaron.Li@outlook.com>
2023-04-01 22:19:47 +0800

    修改bug及优化

--- ClassWork/ClassOne/src/main.cpp
+++ ClassWork/ClassOne/src/main.cpp
@@ -12,9 +12,13 @@ void test_sequenceList() {
     std::cout << "测试顺序表：" << std::endl;
     std::cout << "***************************" << std::endl;
     SqList sqList;
+    /* 初始化顺序表 */
     SequenceList::InitList(&sqList);
+    /* 插入元素 */
     SequenceList::ListInsert(&sqList, 7, 23);
+    /* 查找元素 */
     SequenceList::SearchIndex(&sqList, 23);
+    /* 删除元素 */
     SequenceList::ListDelete(&sqList, 7);
     std::cout << "****************************" << std::endl;
 }
@@ -52,16 +56,18 @@ void test_LinkedList() {
     Node_List node_list1{}, node_list2{};
     List      list{};
     list.InitiateLinkedList();
+    std::cout << "List_a: " << std::endl;
     list.CreateLinked(node_list1);
+    std::cout << "List_b: " << std::endl;
     list.CreateLinked(node_list2);
     list.MergeList_Link(&node_list1, &node_list2);
     std::cout << "***************************" << std::endl;
 }
 
 int main() {
-    test_sequenceList();
-    test_singleLinkedList();
-    test_SeqList();
+    // test_sequenceList();
+    // test_singleLinkedList();
+    // test_SeqList();
     test_LinkedList();
     return 0;
 }
