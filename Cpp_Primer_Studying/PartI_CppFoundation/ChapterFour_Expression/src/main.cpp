163b4d85c2bc2fd444bf10b206fca72260938305
AaronLi <Free_Aaron.Li@outlook.com>
2023-06-03 22:08:15 +0800

    Amendment of Project Name and Copyright Statement

--- Cpp_Primer_Studying/PartI_CppFoundation/ChapterFour_Expression/src/main.cpp
+++ Cpp_Primer_Studying/PartI_CppFoundation/ChapterFour_Expression/src/main.cpp
@@ -2,6 +2,6 @@
 // This program is called:main.
 // If there are no special instructions, this file is used as an exercise and test file.
 
-// Copyright(C) 2023 AaronLi
+// Copyright (C) 2023 AaronLi
 // This program is under the GPL-3.0 license,if you have not received it or the program has a bug,
 // please let me know:<Free_Aaron.Li@outlook.com>.
