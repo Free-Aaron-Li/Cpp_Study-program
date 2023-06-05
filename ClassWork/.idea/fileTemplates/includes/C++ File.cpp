254e28568b43afe269386aae1e29ec4f1656296e
AaronLi <Free_Aaron.Li@outlook.com>
2023-06-06 00:45:33 +0800

    update

--- ClassWork/.idea/fileTemplates/includes/C++ File.cpp
+++
@@ -1,7 +0,0 @@
-/**
- * @author ${USER}
- * @data ${YEAR}-${MONTH}-${DAY}-${TIME}
- * @license GPL-3.0
- */
- 
-
--- ClassWork/.idea/fileTemplates/includes/C++ Source File.cpp
+++
@@ -1,6 +0,0 @@
-/**
- * @author ${USER}
- * @data ${YEAR}-${MONTH}-${DAY}-${TIME}
- * @license GPL-3.0
- */
- 
