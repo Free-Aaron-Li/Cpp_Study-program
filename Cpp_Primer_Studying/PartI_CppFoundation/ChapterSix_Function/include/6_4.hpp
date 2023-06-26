ea430a5c5f29ccf985cfe859c7d1c4d859561d0b
fly_aaron.li@outlook.com <fly_aaron.li@outlook.com>
2023-06-26 18:26:17 +0800

    完成6.3

---
+++ Cpp_Primer_Studying/PartI_CppFoundation/ChapterSix_Function/include/6_4.hpp
@@ -0,0 +1,37 @@
+// This file is part of Cpp_Primer_Studying project.
+// This program is called:6_4.
+// If there are no special instructions, this file is used as an exercise and test file.
+
+// Copyright (C) 2023 AaronLi
+// This program is under the GPL-3.0 license,if you have not received it or the program has a bug,
+// please let me know: <fly_aaron.li@outlook.com>.
+
+#ifndef CPP_PRIMER_STUDYING_6_4_HPP
+#define CPP_PRIMER_STUDYING_6_4_HPP
+
+/**
+ * @title
+ * 6.4 函数重载
+ */
+
+#include "6.hpp"
+
+class Exercise_6_4 {
+ public:
+    /**
+     * @title
+     * 练习6.39
+     * @description
+     * 说明在下面声明中第二条声明语句是何含义。如果有非法的声明，请指出来。
+     * @code
+     * (a) int calc(int,int);
+     *     int calc(const int,const int);
+     * (b) int get();
+     *     double get();
+     * (c) int *reset(int *);
+     *     double *reset(double *);
+     */
+    /* b非法，函数重载需要函数名相同，函数的形参列表中参数数量或者参数类型不尽相同 */
+    static void meaninglessFunction_1();
+};
+#endif  // CPP_PRIMER_STUDYING_6_4_HPP
---
+++ Cpp_Primer_Studying/PartI_CppFoundation/ChapterSix_Function/src/exercise/6_4.cpp
@@ -0,0 +1,10 @@
+// This file is part of Cpp_Primer_Studying project.
+// This program is called:6_4.
+// If there are no special instructions, this file is used as an exercise and test file.
+
+// Copyright (C) 2023 AaronLi
+// This program is under the GPL-3.0 license,if you have not received it or the program has a bug,
+// please let me know: <fly_aaron.li@outlook.com>.
+#include "6_4.hpp"
+
+void Exercise_6_4::meaninglessFunction_1() {}
