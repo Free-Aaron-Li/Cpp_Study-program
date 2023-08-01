ae1f88748369bea0c75f6b9fa0ff693b3e1f159b
aaron <fly_aaron.li@outlook.com>
2023-08-01 22:18:23 +0800

    update 9.2.0

---
+++ Cpp_Primer_Studying/PartII_STL/ChapterNine_SeqContainer/include/9_2.hpp
@@ -0,0 +1,33 @@
+// This file is part of Cpp_Primer_Studying project.
+// This program is called:9_2.
+// If there are no special instructions, this file is used as an exercise and test file.
+
+// Copyright (C) 2023 aaron
+// This program is under the GPL-3.0 license,if you have not received it or the program has a bug,
+// please let me know: <fly_aaron.li@outlook.com>.
+#ifndef CPP_PRIMER_STUDYING_9_2_HPP
+#define CPP_PRIMER_STUDYING_9_2_HPP
+
+/**
+ * @title
+ * 9.2 容器库概览
+ */
+
+#include "9.hpp"
+
+class Exercise_9_2{
+ public:
+    /**
+     * @title
+     * 9.2节练习
+     */
+
+    /**
+     * @title
+     * 练习9.2
+     * @description
+     * 定义一个list对象，其元素类型是int的deque。
+     */
+    static void exercise_9_2();
+};
+#endif  // CPP_PRIMER_STUDYING_9_2_HPP
--- Cpp_Primer_Studying/PartII_STL/ChapterNine_SeqContainer/src/exercise/9_2.cpp
+++ Cpp_Primer_Studying/PartII_STL/ChapterNine_SeqContainer/src/exercise/9_2.cpp
@@ -1,20 +1,10 @@
 // This file is part of Cpp_Primer_Studying project.
-// This program is called:9.
+// This program is called:9_2.
 // If there are no special instructions, this file is used as an exercise and test file.
 
 // Copyright (C) 2023 aaron
 // This program is under the GPL-3.0 license,if you have not received it or the program has a bug,
 // please let me know: <fly_aaron.li@outlook.com>.
-#ifndef CPP_PRIMER_STUDYING_9_HPP
-#define CPP_PRIMER_STUDYING_9_HPP
+#include "9_2.hpp"
 
-/**
- * @title
- * 第9章 顺序容器
- */
-
-#include <iostream>
-#include <list>
-#include <deque>
-
-#endif  // CPP_PRIMER_STUDYING_9_HPP
+void Exercise_9_2::exercise_9_2() { std::list<std::deque<int>> list; }
