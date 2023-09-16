ea5598a2a7de018b0cd98a890a7aeeb03892da4c
aaron <fly_aaron.li@outlook.com>
2023-09-16 20:35:35 +0800

    build the basic framework

---
+++ Cpp_Primer_Studying/PartII_STL/ChapterTen_GenericAlgorithm/include/10.hpp
@@ -0,0 +1,18 @@
+// This file is part of Cpp_Primer_Studying project.
+// This program is called:10.
+// If there are no special instructions, this file is used as an exercise and test file.
+
+// Copyright (C) 2023 aaron
+// This program is under the GPL-3.0 license,if you have not received it or the program has a bug,
+// please let me know: <fly_aaron.li@outlook.com>.
+#ifndef CPP_PRIMER_STUDYING_10_HPP
+#define CPP_PRIMER_STUDYING_10_HPP
+
+/**
+ * @title
+ * 第十章 泛型算法
+ */
+
+#include <iostream>
+
+#endif  // CPP_PRIMER_STUDYING_10_HPP
---
+++ Cpp_Primer_Studying/PartII_STL/ChapterTen_GenericAlgorithm/src/main.cpp
@@ -0,0 +1,7 @@
+// This file is part of Cpp_Primer_Studying project.
+// This program is called:main.
+// If there are no special instructions, this file is used as an exercise and test file.
+
+// Copyright (C) 2023 aaron
+// This program is under the GPL-3.0 license,if you have not received it or the program has a bug,
+// please let me know: <fly_aaron.li@outlook.com>.
